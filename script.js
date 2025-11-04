ea16d7ff87bdb3e57e2f1e4b6ec227ff4c4aba9b
Ben-Kovakas <benjaminkovakas@gmail.com>
2025-11-03 20:28:04 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let isLightOn = true;  // Track the state of the light
+
+function toggleLight() {
+    isLightOn = !isLightOn;  // Toggle the state
+    var pic = isLightOn ? "media/blank.png" : "media/boom.png";
+    document.getElementById('fuji1707').src = pic;
+}
